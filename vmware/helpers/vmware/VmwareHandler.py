618723d34836b3a97be0461c5c65e2e17a67315a
Marco Buratto <m.buratto@lumit.it>
2022-01-26 12:06:54 +0100

    Vmware Handler and Supplicant classes

--- vmware/helpers/vmware/VmwareHandler.py
+++ vmware/helpers/vmware/VmwareHandler.py
@@ -1,6 +1,7 @@
 from vmware.models.VMware.Asset.Asset import Asset
 
 from vmware.helpers.vmware.VmwareSupplicant import VmwareSupplicant
+from vmware.helpers.Exception import CustomException
 from vmware.helpers.Log import Log
 
 
@@ -14,15 +15,43 @@ class VmwareHandler:
         self.moId = moId
         self.oCluster = None
 
+        self.content = None
+
 
 
     ####################################################################################################################
     # Public methods
     ####################################################################################################################
 
-    def getObjects(self, vimType: str) -> dict:
+    def fetchContent(self) -> None:
         try:
+            Log.actionLog("Fetch VMware content.")
+
             supplicant = VmwareSupplicant(Asset(self.assetId).connectionData)
-            return supplicant.getObjects([vimType])
+            connection = supplicant.connect()
+
+            self.content = connection.RetrieveContent()
         except Exception as e:
             raise e
+
+
+
+    # Get objects of type vimType.
+    # vimTypes: vim.VirtualMachine, vim.Folder, vim.Datacenter, vim.VirtualApp, vim.ComputeResource, vim.Network, vim.Datastore.
+    def getObjects(self, vimType: str) -> dict:
+        obj = {}
+
+        try:
+            if not self.content:
+                self.fetchContent()
+
+            if self.content:
+                c = self.content.viewManager.CreateContainerView(self.content.rootFolder, [vimType], True)
+                for managedObject_ref in c.view:
+                    obj[managedObject_ref] = managedObject_ref.name
+            else:
+                raise CustomException(status=400, payload={"VMware": "cannot fetch VMware objects."})
+        except Exception as e:
+            raise e
+
+        return obj
--- vmware/helpers/vmware/VmwareSupplicant.py
+++ vmware/helpers/vmware/VmwareSupplicant.py
@@ -4,7 +4,6 @@ import random
 
 from pyVim.connect import SmartConnect, Disconnect
 
-from vmware.helpers.Exception import CustomException
 from vmware.helpers.Log import Log
 
 
@@ -31,8 +30,6 @@ class VmwareSupplicant(metaclass=Singleton):
             self.password = connectionData["password"]
 
             self.connection = None
-            self.content = None
-
             self.ran = random.random()
         except Exception:
             raise ValueError('Missing key in connection data.')
@@ -43,68 +40,32 @@ class VmwareSupplicant(metaclass=Singleton):
     # Public methods
     ####################################################################################################################
 
-    def fetchContent(self) -> None:
+    def connect(self):
+        if not self.connection:
+            context = ssl.SSLContext(ssl.PROTOCOL_TLS)
+            context.verify_mode = ssl.CERT_NONE
+
+            try:
+                Log.actionLog("["+str(self.ran)+"] Connecting to VMware server: "+str(self.ipAddr))
+
+                self.connection = SmartConnect(
+                    host=self.ipAddr,
+                    user=self.username,
+                    pwd=self.password,
+                    port=self.port,
+                    connectionPoolTimeout=60,
+                    sslContext=context
+                )
+                atexit.register(Disconnect, self.connection)
+            except Exception as e:
+                raise e
+
+        return self.connection
+
+
+
+    def disconnect(self) -> None:
         try:
-            if not self.connection:
-                self.__connect()
-
-            self.content = self.connection.RetrieveContent()
-        except Exception as e:
-            raise e
-
-
-
-    # Get objects of type vimType.
-    # vimTypes: vim.VirtualMachine, vim.Folder, vim.Datacenter, vim.VirtualApp, vim.ComputeResource, vim.Network, vim.Datastore.
-    def getObjects(self, vimType: list = None) -> dict:
-        obj = {}
-        vimType = [] if vimType is None else vimType
-
-        try:
-            if not self.content:
-                Log.actionLog("["+str(self.ran)+"] Get VMware "+str(vimType)+" objects.")
-                self.fetchContent()
-
-            if self.content:
-                c = self.content.viewManager.CreateContainerView(self.content.rootFolder, vimType, True)
-                for managedObject_ref in c.view:
-                    obj[managedObject_ref] = managedObject_ref.name
-            else:
-                raise CustomException(status=400, payload={"VMware": "cannot fetch VMware objects."})
-        except Exception as e:
-            raise e
-
-        return obj
-
-
-
-    ####################################################################################################################
-    # Private methods
-    ####################################################################################################################
-
-    def __connect(self) -> None:
-        context = ssl.SSLContext(ssl.PROTOCOL_TLS)
-        context.verify_mode = ssl.CERT_NONE
-
-        try:
-            Log.actionLog("Connecting to VMware server: "+str(self.ipAddr))
-
-            self.connection = SmartConnect(
-                host=self.ipAddr,
-                user=self.username,
-                pwd=self.password,
-                port=self.port,
-                connectionPoolTimeout=60,
-                sslContext=context
-            )
-            atexit.register(Disconnect, self.connection)
-        except Exception as e:
-            raise e
-
-
-
-    def __disconnect(self) -> None:
-        try:
-            Disconnect(self.content)
+            Disconnect(self.connection)
         except Exception as e:
             raise e
