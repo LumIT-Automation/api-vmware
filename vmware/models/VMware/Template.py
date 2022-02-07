cae3ed5f79f359290432fd54941e099898f02d36
mik <m.sartori@lumit.it>
2022-02-07 12:46:19 +0100

    template

--- vmware/models/VMware/Template.py
+++ vmware/models/VMware/Template.py
@@ -1,5 +1,8 @@
+from typing import List
 from pyVmomi import vim
 
+
+from vmware.models.VMware.backend.VirtualMachine import VirtualMachine as Backend
 from vmware.models.VMware.VirtualMachine import VirtualMachine
 from vmware.models.VMware.Datacenter import Datacenter
 from vmware.models.VMware.Cluster import Cluster
@@ -90,36 +93,16 @@ class VirtualMachineTemplate(VirtualMachine):
     ####################################################################################################################
 
     @staticmethod
-    # Plain vCenter datacenters list.
-    def list(assetId, silent: bool = True) -> dict:
-        tList = []
+    def list(assetId) -> List[dict]:
+        virtualmachines = list()
+
         try:
-            tObjList = VirtualMachineTemplate.listTemplatesOnlyObjects(assetId, silent)
+            for v in Backend.oVirtualMachines(assetId):
+                if v.config.template:
+                    data = VmwareHelper.vmwareObjToDict(v)
+                    virtualmachines.append(data)
 
-            for t in tObjList:
-                tList.append(VmwareHelper.vmwareObjToDict(t))
-
-            return dict({
-                "items": tList
-            })
-
-        except Exception as e:
-            raise e
-
-
-
-    @staticmethod
-    # vCenter templates (not regular virtual machines) pyVmomi objects list.
-    def listTemplatesOnlyObjects(assetId, silent: bool = True) -> list:
-        tObjList = list()
-        try:
-            objList = VirtualMachine.listVirtualMachinesObjects(assetId, silent)
-            for obj in objList:
-                if obj.config.template:
-                    tObjList.append(obj)
-
-            return tObjList
-
+            return virtualmachines
         except Exception as e:
             raise e
 
@@ -130,5 +113,4 @@ class VirtualMachineTemplate(VirtualMachine):
     ####################################################################################################################
 
     def __isVmTemplate(self):
-        config = self.getVirtualMachineConfigObject()
-        return config.template
+        return self.oVirtualMachine.config.template
