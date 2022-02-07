07b3e66b837a37ec64289bf00369b6bb7779a003
mik <m.sartori@lumit.it>
2022-02-07 11:20:22 +0100

    serializers

--- vmware/serializers/VMware/HostSystem.py
+++ vmware/serializers/VMware/HostSystem.py
@@ -2,11 +2,30 @@ from rest_framework import serializers
 
 class VMwareHostSystemSerializer(serializers.Serializer):
     class VMwareHostSystemInnerSerializer(serializers.Serializer):
-        class VMwareHostSystemItemsSerializer(serializers.Serializer):
+        class VMwareHostSystemDatastoresSerializer(serializers.Serializer):
+            moId = serializers.CharField(max_length=255, required=False)
+            name = serializers.CharField(max_length=255, required=False)
+            url = serializers.CharField(max_length=1024, required=False)
+            freeSpace = serializers.IntegerField(required=True)
+            maxFileSize = serializers.IntegerField(required=True)
+            maxVirtualDiskCapacity = serializers.IntegerField(required=True, allow_null=True)
+            type = serializers.CharField(max_length=64, required=False)
+            capacity = serializers.IntegerField(required=True)
+            majorVersion = serializers.IntegerField(required=False)
+            ssd = serializers.BooleanField(required=False)
+            local = serializers.BooleanField(required=False, allow_null=True)
+            multipleHostAccess = serializers.BooleanField(required=False)
+
+        class VMwareHostSystemNetworksSerializer(serializers.Serializer):
             moId = serializers.CharField(max_length=64, required=False)
             name = serializers.CharField(max_length=255, required=False, allow_blank=True)
+            vlanId = serializers.CharField(max_length=15, required=False)
 
-        datastores = VMwareHostSystemItemsSerializer(many=True)
-        networks = VMwareHostSystemItemsSerializer(many=True)
+        assetId = serializers.IntegerField(required=True)
+        moId = serializers.CharField(max_length=64, required=False)
+        name = serializers.CharField(max_length=255, required=False, allow_blank=True)
+        datastores = VMwareHostSystemDatastoresSerializer(many=True)
+        networks = VMwareHostSystemNetworksSerializer(many=True)
+
 
     data = VMwareHostSystemInnerSerializer(required=True)
--- vmware/serializers/VMware/Network.py
+++ vmware/serializers/VMware/Network.py
@@ -3,9 +3,12 @@ from rest_framework import serializers
 class VMwareNetworkSerializer(serializers.Serializer):
     class VMwareNetworkInnerSerializer(serializers.Serializer):
         class VMwareNetworkAttachedHostsSerializer(serializers.Serializer):
+            class VMwareHostNetworksSerializer(serializers.Serializer):
+                vlanId = serializers.CharField(max_length=15, required=False)
+
             moId = serializers.CharField(max_length=64, required=False)
             name = serializers.CharField(max_length=255, required=False)
-            vlanId = serializers.CharField(max_length=15, required=False)
+            networks = VMwareHostNetworksSerializer(required=False, many=True)
 
         assetId = serializers.IntegerField(required=True)
         moId = serializers.CharField(max_length=64, required=False)
