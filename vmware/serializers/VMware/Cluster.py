263a866f9f4d56f292c1e76a2b9c638e8ca3bbe0
Marco Buratto <m.buratto@lumit.it>
2022-02-10 17:17:55 +0100

    Serializers refactor

--- vmware/serializers/VMware/Cluster.py
+++ vmware/serializers/VMware/Cluster.py
@@ -1,18 +1,15 @@
 from rest_framework import serializers
 
+from vmware.serializers.VMware.HostSystem import VMwareHostSystemSerializer
 from vmware.serializers.VMware.Datastore import VMwareDatastoreSerializer
 from vmware.serializers.VMware.Network import VMwareNetworkSerializer
 
 
 class VMwareClusterSerializer(serializers.Serializer):
-    class VMwareClusterItemsSerializer(serializers.Serializer):
-        moId = serializers.CharField(max_length=64, required=True)
-        name = serializers.CharField(max_length=255, required=False)
-
     assetId = serializers.CharField(max_length=64, required=True)
     moId = serializers.CharField(max_length=255, required=False)
     name = serializers.CharField(max_length=255, required=False, allow_blank=True)
 
-    hosts = VMwareClusterItemsSerializer(many=True, required=False, allow_null=True)
+    hosts = VMwareHostSystemSerializer(many=True, required=False, allow_null=True)
     datastores = VMwareDatastoreSerializer(many=True, required=False, allow_null=True)
     networks = VMwareNetworkSerializer(many=True, required=False, allow_null=True)
