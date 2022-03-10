5daa25d61562cc69ac54a62329ff4f82407076fa
mik <m.sartori@lumit.it>
2022-03-10 18:20:47 +0100

    vg rename (begin redhat)

--- vmware/models/Stage2/SshRenameVg.py
+++ vmware/models/Stage2/SshRenameVg.py
@@ -6,20 +6,29 @@ class SshRenameVg(SshCommand):
     def __init__(self, targetId: int, *args, **kwargs):
         super().__init__(targetId, *args, **kwargs)
 
-        getDefaultVg = 'defaultVg=$(vgs -o vg_name | tr -d " " | tail +2);'
+        getDefaultVg = 'defaultVg=$(vgs -o vg_name | tr -d " " | tail -n +2);'
         getVg = 'vgName="vg_`hostname -s`";'
         getSwapDevs = 'swapDevs=$(grep " swap " /etc/fstab | cut -d" " -f1);'
-        swapOff = 'for dev in $swapDevs; do if ! swapoff $dev; then exit 11; fi; done;'
+        swapOff = 'for dev in $swapDevs; do swapoff $dev; done;'
         vgRename = 'if ! vgrename $defaultVg $vgName; then exit 13; fi;'
         vgChange = 'vgchange -ay || exit 15;'
         updateFstab = 'sed -i -e "s#/${defaultVg}#/${vgName}#g" /etc/fstab;'
-        getLvs = 'lVs=$(lvs -o lv_name | tr -d " " | tail +2);'
+        getLvs = 'lVs=$(lvs -o lv_name | tr -d " " | tail -n +2);'
         refreshLvs = 'for lv in $lVs; do if ! lvchange /dev/${vgName}/$lv --refresh; then exit 17; fi; done;'
+
+        debUpdateGrubCfg = '[ -w /boot/grub/grub.cfg ] && sed -i -e "s#/${defaultVg}#/${vgName}#" /boot/grub/grub.cfg;'
         debUpdateInitrdCfg = '[ -w /etc/initramfs-tools/conf.d/resume ] && sed -i -e "s#/${defaultVg}#/${vgName}#" /etc/initramfs-tools/conf.d/resume;'
-        debUpdateGrub = '[ -w /boot/grub/grub.cfg ] && sed -i -e "s#/${defaultVg}#/${vgName}#" /boot/grub/grub.cfg;'
         debUpdateInitrd = '[ -x /sbin/update-initramfs ] && if ! update-initramfs -u; then exit 19; fi;'
+
+        rhGetRootDefaultLv = 'defaultRoot=$(grep " / " /etc/fstab | cut -f1 -d" ");' # Full path lv for root mount point (/dev/mapper/...-root
+        rhGetRootLv = 'lvRoot=$(for lv in $A;do if grep " / " /etc/fstab | grep -q $lv;then echo $lv;fi;done);' # lv name for root mount point
+        rhUpdateGrubCfg = '[ -w /boot/grub2/grub.cfg ] && sed -i -e "s#=${defaultVg}/#=${vgName}/#g" -e "s#${defaultRoot}#/dev/${vgName}/${lvRoot}g" /boot/grub2/grub.cfg;'
+        rhUpdateGrubDefault = '[ -w /etc/default/grub ] && sed -i -e "s#=${defaultVg}/#=${vgName}/#g" /etc/default/grub;'
+        rhUpdateGrubEnv = '[ -w /boot/grub2/grubenv ] && sed -i -e "s#=${defaultVg}/#=${vgName}/#g" -e "s#${defaultRoot}#/dev/${vgName}/${lvRoot}g /boot/grub2/grubenv;'
+
         reboot = 'reboot'
 
-        self.command = getDefaultVg + getVg + getSwapDevs + swapOff + vgRename + vgChange + updateFstab + getLvs + \
-            refreshLvs + debUpdateGrub + debUpdateInitrdCfg + debUpdateInitrd + reboot
+        self.command = getDefaultVg + getVg + getSwapDevs + swapOff + vgRename + vgChange + updateFstab #+ getLvs + \
+        #    refreshLvs + debUpdateGrubCfg + rhGetRootDefaultLv + rhGetRootLv + rhUpdateGrubCfg + rhUpdateGrubEnv + \
+        #    rhUpdateGrubDefault + debUpdateInitrdCfg + debUpdateInitrd + reboot
 
--- vmware/models/Stage2/SshResizePartition.py
+++ vmware/models/Stage2/SshResizePartition.py
@@ -11,7 +11,8 @@ class SshResizePartition(SshCommand):
         getLastPartNumber = 'N=0; for n in $pNum; do if [ $n -gt $N ]; then N=$n;fi;done;'
         echoLastPart = 'echo "partition: /dev/${diskDev}${N}";'
         checkPartIsPv = 'if ! pvs /dev/${diskDev}${N} > /dev/null 2>&1; then exit 5; fi;'
-        lastPartResize = 'if ! printf "d\n${N}\nn\np\n${N}\n\n\nt\n${N}\n8e\nw\n" | fdisk /dev/${diskDev}; then exit 7; fi;'
+        # lastPartResize = 'if ! printf "d\n${N}\nn\np\n${N}\n\n\nt\n${N}\n8e\nw\n" | fdisk /dev/${diskDev}; then exit 7; fi;'
+        lastPartResize = 'printf "d\n${N}\nn\np\n${N}\n\n\nt\n${N}\n8e\nw\n" | fdisk /dev/${diskDev}; true;' # workaround for redhat 7.
         partProbe = 'partprobe; sleep 1;'
         pvResize = 'if ! pvresize /dev/${diskDev}${N}; then exit 9; fi;'
 
