e392dfd285033c6374d317b7e84cb482781e0a61
Marco Buratto <m.buratto@lumit.it>
2022-03-17 11:39:09 +0100

    VmwareSupplicant

--- vmware/helpers/vmware/VmwareSupplicant.py
+++ vmware/helpers/vmware/VmwareSupplicant.py
@@ -7,17 +7,7 @@ from pyVim.connect import SmartConnect, Disconnect
 from vmware.helpers.Log import Log
 
 
-class Singleton(type):
-    _instances = {}
-
-    def __call__(cls, *args, **kwargs):
-        if cls not in cls._instances:
-            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
-        return cls._instances[cls]
-
-
-
-class VmwareSupplicant(metaclass=Singleton):
+class VmwareSupplicant:
     def __init__(self, connectionData, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
