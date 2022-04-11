913374ef230b86f9e85f0776f8ac2eb07f39a526
Marco Buratto <m.buratto@lumit.it>
2022-04-11 18:08:26 +0200

    Correct Target model

--- vmware/serializers/Stage2/Target.py
+++ vmware/serializers/Stage2/Target.py
@@ -4,19 +4,21 @@ from vmware.serializers.Stage2.FinalPubKey import Stage2FinalPubKeySerializer
 
 
 class Stage2TargetSerializer(serializers.Serializer):
-    id = serializers.IntegerField(required=False)
-    ip = serializers.IPAddressField(required=False)
-    port = serializers.IntegerField(required=False)
-    api_type = serializers.CharField(required=True, max_length=64, allow_blank=True)
-    id_bootstrap_key = serializers.IntegerField(required=False, allow_null=True)
-    username = serializers.CharField(required=False, max_length=64, allow_blank=True)
-    password = serializers.CharField(required=False, max_length=64, allow_blank=True)
-    id_asset = serializers.IntegerField(required=False, allow_null=True)
+    class Stage2TargetSerializerConnection(serializers.Serializer):
+        ip = serializers.IPAddressField(required=False)
+        port = serializers.IntegerField(required=False)
+        api_type = serializers.CharField(required=True, max_length=64, allow_blank=True)
+        id_bootstrap_key = serializers.IntegerField(required=False, allow_null=True)
+        username = serializers.CharField(required=False, max_length=64, allow_blank=True)
+        password = serializers.CharField(required=False, max_length=64, allow_blank=True)
 
+    id = serializers.IntegerField(required=False)
+    id_asset = serializers.IntegerField(required=False, allow_null=True)
     task_moid = serializers.CharField(required=False, max_length=64, allow_blank=True, allow_null=True)
     task_progress = serializers.IntegerField(required=False, allow_null=True)
     task_startTime = serializers.CharField(required=False, max_length=64, allow_blank=True, allow_null=True)
     task_queueTime = serializers.CharField(required=False, max_length=64, allow_blank=True, allow_null=True)
     vm_name = serializers.CharField(required=False, max_length=128, allow_blank=True)
+    connection = Stage2TargetSerializerConnection(required=False)
 
     final_pubkeys = Stage2FinalPubKeySerializer(many=True, required=False, allow_null=True)
