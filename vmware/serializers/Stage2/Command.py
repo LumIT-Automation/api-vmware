4fa164bdd3fc9bb3838ca621a20a73168d16e4f6
mik <m.sartori@lumit.it>
2022-04-12 15:41:54 +0200

    add/patch command: base64 encoded command

--- vmware/serializers/Stage2/Command.py
+++ vmware/serializers/Stage2/Command.py
@@ -5,3 +5,4 @@ class Stage2CommandSerializer(serializers.Serializer):
     uid = serializers.CharField(required=True)
     command = serializers.CharField(required=True, max_length=65535)
     template_args = serializers.JSONField(required=True)
+    base64 = serializers.BooleanField(required=False, allow_null=True)
