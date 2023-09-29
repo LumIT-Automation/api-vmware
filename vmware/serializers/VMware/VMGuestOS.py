4ce30d214ee1fe6e3f73d6718d65def72f80cbdb
mik <m.sartori@lumit.it>
2023-09-29 16:06:27 +0200

    serializer fix

--- vmware/serializers/VMware/VMGuestOS.py
+++ vmware/serializers/VMware/VMGuestOS.py
@@ -5,7 +5,7 @@ class VMwareVMGuestOSSerializer(serializers.Serializer):
 
     class VMwareVMGuestOSToolsSerializer(serializers.Serializer):
         version = serializers.IntegerField(required=False)
-        type = serializers.CharField(required=False, allow_blank=True, max_length=64)
+        type = serializers.CharField(required=False, allow_blank=True, allow_null=True, max_length=64)
         status = serializers.CharField(required=False, allow_blank=True, max_length=64)
         runningStatus = serializers.CharField(required=False, allow_blank=True, max_length=64)
 
@@ -14,7 +14,7 @@ class VMwareVMGuestOSSerializer(serializers.Serializer):
         searchDomain = serializers.ListField(child=serializers.CharField(max_length=255), required=False)
         dnsList = serializers.ListField(child=serializers.CharField(max_length=64), required=False)
 
-    hostname = serializers.CharField(required=False, allow_blank=True, max_length=255)
+    hostname = serializers.CharField(required=False, allow_blank=True,  allow_null=True, max_length=255)
     guestOS = serializers.CharField(required=False, allow_blank=True, allow_null=True, max_length=255)
     guestOSFamily = serializers.CharField(required=False, allow_blank=True, allow_null=True, max_length=64)
     vmTools = VMwareVMGuestOSToolsSerializer(required=False)
